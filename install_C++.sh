6b97ec3ecc88758844c9b69c54a98b11de6afbd6
Kenneth Benzie <k.benzie83@gmail.com>
2013-12-05 23:53:31 +0000

    Removed crappy scripts

--- install_C++.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd C++
-zip -f ~/.config/sublime-text-3/Installed\ Packages/C++.sublime-package *
--- uninstall_C++.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm ~/.config/sublime-text-3/Installed\ Packages/C++.sublime-package
