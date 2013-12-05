0fe03f92477bf7850a4fd73b2db2f928b2f7e2b7
Kenneth Benzie <k.benzie@codeplay.com>
2013-12-05 18:11:39 +0000

    Added modified C++ sublime text language support

---
+++ install_C++.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd C++
+zip -f ~/.config/sublime-text-3/Installed\ Packages/C++.sublime-package *
---
+++ uninstall_C++.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm ~/.config/sublime-text-3/Installed\ Packages/C++.sublime-package
